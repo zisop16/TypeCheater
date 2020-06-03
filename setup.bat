a6df5136259efbb227c2566d56151b3756222d41
zisop <800017643@fjuhsd.org>
2020-06-02 19:33:04 -0700

    More error handling, fixed bugs, better UI

---
+++ setup.bat
@@ -0,0 +1,4 @@
+@echo off
+color 0b
+pip install -r requirements.txt
+@echo on
--- tpBOT.py
+++ tpBOT.py
@@ -7,25 +7,28 @@ from PIL import Image
 import time
 import pytesseract
 import requests
-from CaptchaSolve import CaptchaSolver
+import math
+from .CaptchaSolve import CaptchaSolver
 
-if __name__ == '__main__':
+class type_racer:
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--log-level=OFF")
+        self.driver = webdriver.Chrome(options=chrome_options)
+        min_path = 7
+        max_path = 12
+        self.possible_captcha_paths = range(min_path, max_path + 1, 1)
 
-    chrome_options = Options()
-    chrome_options.add_argument("--log-level=OFF")
-    driver = webdriver.Chrome(options = chrome_options)
-
-
-    def log_in():
+    def log_in(self):
         """
         Logs in by looking at account.txt for an account
         """
         main_url = "https://play.typeracer.com/"
-        driver.get(main_url)
+        self.driver.get(main_url)
         signin_path = "/html/body/div[1]/table/tbody/tr/td[3]/div/table/tbody/tr[2]/td[1]/table/tbody/tr/td[1]/a"
         while True:
             try:
-                signin_element = driver.find_element_by_xpath(signin_path)
+                signin_element = self.driver.find_element_by_xpath(signin_path)
                 break
             except NoSuchElementException:
                 time.sleep(1)
@@ -45,14 +48,14 @@ if __name__ == '__main__':
                   " Alternatively, play as a guest by using Username and no colon")
             with open("account.txt", "w") as account_file:
                 account_file.write("SampleUsername:SamplePassword")
-                driver.quit()
+                self.driver.quit()
                 quit()
         if as_guest:
             print("Found no password in accounts.txt... playing as a guest with name inputted")
             while True:
                 try:
                     guest_path = "/html/body/div[5]/div/div/div[3]/div/div[1]/div/table[3]/tbody/tr[1]/td/a/table/tbody/tr/td[2]"
-                    guest_elem = driver.find_element_by_xpath(guest_path)
+                    guest_elem = self.driver.find_element_by_xpath(guest_path)
                     guest_elem.click()
                     break
                 except NoSuchElementException:
@@ -60,7 +63,7 @@ if __name__ == '__main__':
             nickname_path = "/html/body/div[5]/div/div/div[3]/div/div[1]/div/table[3]/tbody/tr[2]/td/div/table/tbody/tr[1]/td/table/tbody/tr/td[2]/input"
             while True:
                 try:
-                    nickname_elem = driver.find_element_by_xpath(nickname_path)
+                    nickname_elem = self.driver.find_element_by_xpath(nickname_path)
                     break
                 except NoSuchElementException:
                     time.sleep(1)
@@ -73,53 +76,41 @@ if __name__ == '__main__':
             nickname_elem.send_keys(username)
             time.sleep(1)
             apply_path = "/html/body/div[5]/div/div/div[3]/div/div[1]/div/table[3]/tbody/tr[2]/td/div/table/tbody/tr[1]/td/table/tbody/tr/td[3]/button"
-            apply_elem = driver.find_element_by_xpath(apply_path)
+            apply_elem = self.driver.find_element_by_xpath(apply_path)
             apply_elem.click()
         else:
             user_path = "/html/body/div[5]/div/div/div[3]/div/div[1]/div/table[1]/tbody/tr[2]/td/div/table/tbody/tr[1]/td[2]/input"
             pass_path = "/html/body/div[5]/div/div/div[3]/div/div[1]/div/table[1]/tbody/tr[2]/td/div/table/tbody/tr[2]/td[2]/table/tbody/tr[1]/td/input"
             while True:
                 try:
-                    username_elem = driver.find_element_by_xpath(user_path)
-                    password_elem = driver.find_element_by_xpath(pass_path)
+                    username_elem = self.driver.find_element_by_xpath(user_path)
+                    password_elem = self.driver.find_element_by_xpath(pass_path)
                     break
                 except NoSuchElementException:
                     time.sleep(1)
             username_elem.send_keys(username)
             password_elem.send_keys(password)
             enter_path = "/html/body/div[5]/div/div/div[3]/div/div[1]/div/table[1]/tbody/tr[2]/td/div/table/tbody/tr[4]/td[2]/table/tbody/tr/td[1]/button"
-            enter_elem = driver.find_element_by_xpath(enter_path)
+            enter_elem = self.driver.find_element_by_xpath(enter_path)
             enter_elem.click()
 
-    def enter_race():
+    def enter_race(self):
         """
         Will enter the race assuming that we are at the login page
         """
         while True:
             try:
                 racestart_path = "/html/body/div[1]/div/div[1]/div[2]/table/tbody/tr[2]/td[2]/div/div[1]/div/table/tbody/tr[2]/td/table/tbody/tr/td[2]/table/tbody/tr[1]/td/a"
-                racestart_elem = driver.find_element_by_xpath(racestart_path)
+                racestart_elem = self.driver.find_element_by_xpath(racestart_path)
                 break
             except ElementNotInteractableException:
                 time.sleep(1)
         racestart_elem.click()
 
-
-    def complete_race():
+    def complete_race(self):
         """
         Completes a race assuming that the player is currently at the race page
         """
-
-        try:
-            # A no thanks button might appear for joining instead of being a guest
-            # If we are running the program as a guest, so we will look for it
-            # And click it if it exists
-            nothanks_path = r"/html/body/div[7]/div/div/div[3]/div/div[2]/a"
-            nothanks_elem = driver.find_element_by_xpath(nothanks_path)
-            nothanks_elem.click()
-        except NoSuchElementException:
-            pass
-        time.sleep(1.5)
 
         def find_solution():
             # There are two text containers that may or may not appear,
@@ -132,20 +123,20 @@ if __name__ == '__main__':
                     # If these two elements don't exist, the page hasn't loaded yet, so we sleep and reiterate
                     # The loop
                     firsttext_path = r"/html/body/div[1]/div/div[1]/div[2]/table/tbody/tr[2]/td[2]/div/div[1]/div/table/tbody/tr[2]/td[3]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/div/span[1]"
-                    firsttext_elem = driver.find_element_by_xpath(firsttext_path)
+                    firsttext_elem = self.driver.find_element_by_xpath(firsttext_path)
                     first_text = firsttext_elem.text
                     secondtext_path = r"/html/body/div[1]/div/div[1]/div[2]/table/tbody/tr[2]/td[2]/div/div[1]/div/table/tbody/tr[2]/td[3]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/div/span[2]"
-                    secondtext_elem = driver.find_element_by_xpath(secondtext_path)
+                    secondtext_elem = self.driver.find_element_by_xpath(secondtext_path)
                     second_text = secondtext_elem.text
                     try:
                         # There is a possibility of being 3 or 4 text elements storing text,
                         # So we poll these elements to see if they exist
                         thirdtext_path = r"/html/body/div[1]/div/div[1]/div[2]/table/tbody/tr[2]/td[2]/div/div[1]/div/table/tbody/tr[2]/td[3]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/div/span[3]"
-                        thirdtext_elem = driver.find_element_by_xpath(thirdtext_path)
+                        thirdtext_elem = self.driver.find_element_by_xpath(thirdtext_path)
                         third_text = thirdtext_elem.text
                         try:
                             fourthtext_path = r"/html/body/div[1]/div/div[1]/div[2]/table/tbody/tr[2]/td[2]/div/div[1]/div/table/tbody/tr[2]/td[3]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/div/span[4]"
-                            fourthtext_elem = driver.find_element_by_xpath(fourthtext_path)
+                            fourthtext_elem = self.driver.find_element_by_xpath(fourthtext_path)
                             fourth_text = fourthtext_elem.text
                         except NoSuchElementException:
                             found_fourth = False
@@ -178,20 +169,9 @@ if __name__ == '__main__':
                 solution_text = "%s %s" % (first_text, second_text)
                 texts = (first_text, second_text)
             return solution_text, texts
+
         # We use find solution to find a solution to the race
         solution_text, texts = find_solution()
-        time.sleep(1)
-        # We create a test solution for comparison, to make sure we pulled the correct solution
-        # And that the solution didn't change while find_solution was called
-        test_solution, test_texts = find_solution()
-        while solution_text != test_solution:
-            # If the solution did change, we will try finding the solution again until we get
-            # Consistent results
-            solution_text = test_solution
-            texts = test_texts
-            time.sleep(.2)
-            test_solution = find_solution()
-
         text_num = 1
         for text in texts:
             print("Text %i: (%s)" % (text_num, text))
@@ -201,13 +181,22 @@ if __name__ == '__main__':
         while True:
             try:
                 raceenter_path = r"/html/body/div[1]/div/div[1]/div[2]/table/tbody/tr[2]/td[2]/div/div[1]/div/table/tbody/tr[2]/td[3]/table/tbody/tr[2]/td/table/tbody/tr[2]/td/input"
-                raceenter_elem = driver.find_element_by_xpath(raceenter_path)
-                for char in solution_text:
-                    raceenter_elem.send_keys(char)
-                    if char == " ":
-                        new_start = solution_text.index(" ") + 1
-                        solution_text = solution_text[new_start : len(solution_text)]
-                    time.sleep(.01)
+                raceenter_elem = self.driver.find_element_by_xpath(raceenter_path)
+                num_chars = 5
+                num_words = int(math.ceil(len(solution_text) / num_chars))
+                words = []
+                wait_time = .15
+                for word_num in range(num_words):
+                    start_ind = word_num * num_chars
+                    end_ind = start_ind + num_chars
+                    words.append(solution_text[start_ind: end_ind])
+                for word in words:
+                    start_time = time.time()
+                    raceenter_elem.send_keys(word)
+                    end_time = time.time()
+                    elapsed = end_time - start_time
+                    curr_wait = max(wait_time - elapsed, 0)
+                    time.sleep(curr_wait)
                 try:
                     raceenter_elem.send_keys(" ")
                 except ElementNotInteractableException:
@@ -218,22 +207,22 @@ if __name__ == '__main__':
             except StaleElementReferenceException:
                 continue
 
-    def handle_captcha():
+    def handle_captcha(self):
         """
         Handles the possibility of a captcha
         assuming that the player is finished with a race
         """
         try:
-            begincaptcha_path = r"/html/body/div[7]/div/div/div[2]/div/div/table/tbody/tr[4]/td/button"
-            second_begincaptcha = r"/html/body/div[8]/div/div/div[2]/div/div/table/tbody/tr[4]/td/button"
-            third_begincaptcha = r"/html/body/div[9]/div/div/div[2]/div/div/table/tbody/tr[4]/td/button"
-            try:
-                begincaptcha_elem = driver.find_element_by_xpath(begincaptcha_path)
-            except NoSuchElementException:
-                try:
-                    begincaptcha_elem = driver.find_element_by_xpath(second_begincaptcha)
-                except NoSuchElementException:
-                    begincaptcha_elem = driver.find_element_by_xpath(third_begincaptcha)
+            for path_num in self.possible_captcha_paths:
+                begincaptcha_path = r"/html/body/div[%i]/div/div/div[2]/div/div/table/tbody/tr[4]/td/button" % path_num
+                if path_num != self.possible_captcha_paths[len(self.possible_captcha_paths) - 1]:
+                    try:
+                        begincaptcha_elem = self.driver.find_element_by_xpath(begincaptcha_path)
+                        break
+                    except NoSuchElementException:
+                        pass
+                else:
+                    begincaptcha_elem = self.driver.find_element_by_xpath(begincaptcha_path)
             begincaptcha_elem.click()
             found_captcha = True
         except NoSuchElementException:
@@ -245,21 +234,20 @@ if __name__ == '__main__':
             def extract_captcha():
 
                 def attempt_find():
-                    possible_paths = (8, 9, 10)
-                    correct_num = None
-                    for path_num in possible_paths:
+                    correct_path = None
+                    for path_num in self.possible_captcha_paths:
                         captcha_path = r"/html/body/div[%i]/div/div/div[2]/div/div/table/tbody/tr[3]/td/img" % path_num
-                        if path_num != possible_paths[len(possible_paths) - 1]:
+                        if path_num != self.possible_captcha_paths[len(self.possible_captcha_paths) - 1]:
                             try:
-                                captcha_elem = driver.find_element_by_xpath(captcha_path)
-                                correct_num = path_num
+                                captcha_elem = self.driver.find_element_by_xpath(captcha_path)
+                                correct_path = path_num
                                 break
                             except NoSuchElementException:
                                 pass
                         else:
-                            captcha_elem = driver.find_element_by_xpath(captcha_path)
-                            correct_num = path_num
-                    return captcha_elem, correct_num
+                            captcha_elem = self.driver.find_element_by_xpath(captcha_path)
+                            correct_path = path_num
+                    return captcha_elem, correct_path
 
                 while True:
                     try:
@@ -281,51 +269,79 @@ if __name__ == '__main__':
                 captcha_textbox_path = r"/html/body/div[%i]/div/div/div[2]/div/div/table/tbody/tr[4]/td/textarea" % path_num
                 while True:
                     try:
-                        captcha_textbox_elem = driver.find_element_by_xpath(captcha_textbox_path)
+                        captcha_textbox_elem = self.driver.find_element_by_xpath(captcha_textbox_path)
                         captcha_textbox_elem.send_keys(solution)
                         break
                     except (ElementNotInteractableException, NoSuchElementException):
                         time.sleep(1)
 
                 submit_path = r"/html/body/div[%i]/div/div/div[2]/div/div/table/tbody/tr[5]/td/table/tbody/tr/td[2]/button" % path_num
-                submit_elem = driver.find_element_by_xpath(submit_path)
+                submit_elem = self.driver.find_element_by_xpath(submit_path)
                 submit_elem.click()
-                return path_num
 
             while True:
-                path_num = solve_captcha()
-                time.sleep(2)
-                try:
+                solve_captcha()
+                time.sleep(1)
+                was_closed = False
+                for path_num in self.possible_captcha_paths:
                     restart_path = r"/html/body/div[%i]/div/div/div[3]/div/div/table/tbody/tr[3]/td/table/tbody/tr/td[2]/button" % path_num
-                    restart_elem = driver.find_element_by_xpath(restart_path)
-                    restart_elem.click()
-                    time.sleep(2)
-                except NoSuchElementException:
+                    close_path = r"/html/body/div[%i]/div/div/div[1]" % path_num
+                    try:
+                        restart_elem = self.driver.find_element_by_xpath(restart_path)
+                        restart_elem.click()
+                        break
+                    except NoSuchElementException:
+                        try:
+                            close_elem = self.driver.find_element_by_xpath(close_path)
+                            close_elem.click()
+                            was_closed = True
+                            break
+                        except NoSuchElementException:
+                            pass
+                if was_closed:
                     break
-            close_path = r"/html/body/div[%i]/div/div/div[1]" % path_num
-            close_elem = driver.find_element_by_xpath(close_path)
-            close_elem.click()
+                time.sleep(1)
 
+    def handle_popup(self):
+        possible_paths = range(7, 13, 1)
+        for path_num in possible_paths:
+            nothanks_path = r"/html/body/div[%i]/div/div/div[3]/div/div[2]/a" % path_num
+            try:
+                # A no thanks button might appear for joining instead of being a guest
+                # If we are running the program as a guest, so we will look for it
+                # And click it if it exists
+                nothanks_elem = self.driver.find_element_by_xpath(nothanks_path)
+                nothanks_elem.click()
+            except NoSuchElementException:
+                pass
 
-    print("Logging in...")
-    log_in()
-    print("Entering race...")
-    enter_race()
-    print("Completing race...")
-    complete_race()
-    time.sleep(4)
-    handle_captcha()
-    time.sleep(2)
-    for i in range(5):
+    def initialize_racer(self):
+        print("Logging in...")
+        self.log_in()
+        print("Entering race...")
+        self.enter_race()
+
+    def race(self):
+        print("Completing race...")
+        self.complete_race()
+        time.sleep(1)
+        print("Handling captcha...")
+        self.handle_captcha()
+        time.sleep(1)
+
+    def restart_race(self):
         print("Restarting race...")
         while True:
             try:
                 raceagain_path = r"/html/body/div[1]/div/div[1]/div[2]/table/tbody/tr[2]/td[2]/div/div[1]/div/table/tbody/tr[2]/td[3]/table/tbody/tr[3]/td/table/tbody/tr/td[2]/a"
-                raceagain_elem = driver.find_element_by_xpath(raceagain_path)
+                raceagain_elem = self.driver.find_element_by_xpath(raceagain_path)
                 # We click the "start a new race" button after the race is over
                 raceagain_elem.click()
                 # We will try clicking it again in case there was a pop up
                 # Asking us to make an account
+                time.sleep(1)
+                self.handle_popup()
+                time.sleep(1)
                 try:
                     raceagain_elem.click()
                 except StaleElementReferenceException:
@@ -334,18 +350,20 @@ if __name__ == '__main__':
                     pass
                 break
             except ElementClickInterceptedException:
-                time.sleep(2)
-        time.sleep(2)
-        print("Completing race...")
-        complete_race()
-        time.sleep(2)
-        print("Handling captcha...")
-        handle_captcha()
-        time.sleep(2)
+                time.sleep(1)
+        time.sleep(1)
 
+    def quit(self):
+        self.driver.quit()
 
+if __name__ == '__main__':
 
-
+    racer = type_racer()
+    racer.initialize_racer()
+    racer.race()
+    for i in range(5):
+        racer.restart_race()
+        racer.race()
 
     input("Exit")
-    driver.quit()
+    racer.quit()
